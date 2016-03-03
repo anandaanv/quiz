db97a5daa3a39f73ce7f6e2276eddc1de09614c2
Anand Vaidya <anand1711@gmail.com>
2016-03-03 15:31:56 +0530

    Update Parser.java
    1. Changed file reading logic to handle large files.
    2. refactored the code to make it more readable.
    3. Addressed abstraction issues like customized exception, getter setters for member variable "file" etc.
    4. cleaned up open streams. using autoclosable.
    
    Things to be done - 
    1. I still feel that there is some room to improvise the file reading logic. Definitely BUFFER_SIZE is just one thing. buthave to explore if something can help to filter unicode characters, like using java8 IntStream. In case of large files, it can help introducing parallelism.
    2. Few changes could be done to use polymorphism to handle unicode strings. That would help reduce one check in "if"
    3. Ofcourse I did not spend time in adding comments. Ideally I should have.

--- Parser.java
+++ Parser.java
@@ -1,42 +1,83 @@
+package test;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
-    }
-    return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
-  }
+	
+	private static final int BUFFER_SIZE = 1000;
+	private File file;
+	
+	public Parser(File file) {
+		super();
+		this.file = file;
+	}
+	
+	/** We should not be having these getter setters. ideally we should be setting that data in the constructor
+	 */
+//	public synchronized void setFile(File f) {
+//		file = f;
+//	}
+//	public synchronized File getFile() {
+//		return file;
+//	}
+	public String getContent(boolean isUnicode) throws ParserException {
+		try (FileReader ipStream = new FileReader(file)){
+			StringBuilder builder = new StringBuilder();
+			
+			CharBuffer buf = CharBuffer.allocate(BUFFER_SIZE);
+			while (ipStream.read(buf) > 0) {
+				processBuffer(isUnicode, builder, buf);
+			}
+			return builder.toString();
+		}catch(IOException e){
+			throw new ParserException("Exception in reading file", e);
+		}
+	}
+
+	private void processBuffer(boolean isUnicode, StringBuilder builder, CharBuffer buf) {
+		while(buf.hasRemaining()){
+			char val = buf.get();
+			if (isValid(isUnicode, val)) {
+				builder.append(val);
+			}
+		}
+	}
+	
+	private boolean isValid(boolean isUnicode, int data) {
+		return !isUnicode || data < 0x80;
+	}
+
+	// We do not need this method. Just that its public, so could be access by others, so havent deleted the same.
+	public String getContentWithoutUnicode() throws ParserException {
+		return getContent(true);
+	}
+
+
+	public void saveContent(String content) throws ParserException {
+		try (FileOutputStream o = new FileOutputStream(file)){
+			for (int i = 0; i < content.length(); i += 1) {
+				o.write(content.charAt(i));
+			}
+		} catch (IOException e) {
+			throw new ParserException("exception in writing", e);
+		}
+	}
+
+	public static class ParserException extends Exception{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public ParserException(String arg0, Throwable arg1) {
+			super(arg0, arg1);
+		}
+
+	}
 }
